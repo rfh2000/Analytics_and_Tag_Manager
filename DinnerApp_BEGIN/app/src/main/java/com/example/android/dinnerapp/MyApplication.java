39319d18d3a5b8b0577a6276770290dcfda3af70
Richard <rick_pendlebury@hotmail.com>
2016-09-04 15:15:27 +0100

    Update myapplication subclass to start tracking

--- DinnerApp_BEGIN/app/src/main/java/com/example/android/dinnerapp/MyApplication.java
+++ DinnerApp_BEGIN/app/src/main/java/com/example/android/dinnerapp/MyApplication.java
@@ -2,8 +2,39 @@ package com.example.android.dinnerapp;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Logger;
+import com.google.android.gms.analytics.Tracker;
+
 /**
  * Created by R.Pendlebury on 04/09/2016.
  */
 public class MyApplication extends Application {
+
+    Tracker mTracker;
+
+    public void startTracking() {
+        if (mTracker == null) {
+            GoogleAnalytics ga = GoogleAnalytics.getInstance(this);
+
+            // Get the config data for the tracker
+            mTracker = ga.newTracker(R.xml.ga_tracker);
+
+            // Enable tracking of activities
+            ga.enableAutoActivityReports(this);
+
+            // Set the log level to verbose.
+            ga.getLogger()
+                    .setLogLevel(Logger.LogLevel.VERBOSE);
+        }
+    }
+
+    public Tracker getTracker() {
+        // Make sure the tracker exists
+        startTracking();
+
+        // Then return the tracker
+        return mTracker;
+    }
+
 }
